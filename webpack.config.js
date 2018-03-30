507ade005a060e1e90cdee2bc21549eab1d55954
zhongds <zhongds@sutpc.com>
2018-03-30 09:51:32 +0800

    change entry filename

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: {
+    index: path.resolve(__dirname, 'index.js')
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
