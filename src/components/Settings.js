18a6fb023882bdddd4b72b5ecf7c08cc28a34edf
zhongds <zhongds@sutpc.com>
2018-04-10 17:55:04 +0800

    add webpack build analysis

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import moment from 'moment';
+import _ from 'lodash'
 import log from 'utils/lg';
 
 export default function Settings() {
   log('===================settings');
+  moment().format('MMMM Do YYYY, h:mm:ss a');
+  _.compact([0, 1, false, 2, '', 3]);
   return (
     <div>Settings.....</div>
   )
